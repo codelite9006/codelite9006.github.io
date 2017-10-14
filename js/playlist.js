909b7f1f113181c54702c67bae651693ff90eb65
codelite9006 <codelite9006@gmail.com>
2017-10-14 12:12:27 +0800

    Create playlist.js

---
+++ js/playlist.js
@@ -0,0 +1,21 @@
+var arr_pg1 = [];
+var root = "file:///sdcard/Download/歌曲/";
+arr_pg1.push(root+"我呸.mp3");
+arr_pg1.push("https://img3.doubanio.com/lpic/s29074324.jpg");
+arr_pg1.push("我呸");
+arr_pg1.push("蔡依林");
+
+arr_pg1.push(root+"舞孃.mp3");
+arr_pg1.push("https://i.pinimg.com/736x/d0/78/9e/d0789e077d08f341bfbfef9ecc416a93--jolin-tsai-album-covers.jpg");
+arr_pg1.push("舞孃");
+arr_pg1.push("蔡依林");
+
+arr_pg1.push(root+"Shall Never Surrender.mp3");
+arr_pg1.push("https://i.ytimg.com/vi/xIIYt5lcq3A/hqdefault.jpg");
+arr_pg1.push("Shall Never Surrender");
+arr_pg1.push("DMC 4 Soundtrack");
+
+arr_pg1.push(root+"Faded.mp3");
+arr_pg1.push("https://i.ytimg.com/vi/_Qo3SrDQCL4/maxresdefault.jpg");
+arr_pg1.push("Faded");
+arr_pg1.push("Alan Walker");

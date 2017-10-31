7c1f6343087b161e6c49dedde92f425802f832c4
codelite9006 <codelite9006@gmail.com>
2017-11-01 04:10:55 +0800

    Create private.js

---
+++ js/private.js
@@ -0,0 +1,29 @@
+//song list add here
+//music url, album image, song name, artist 
+var arr_pg1 = [];
+var root = "file:///sdcard/Download/歌曲/";
+var art = "file:///sdcard/Download/歌曲/照片";
+
+arr_pg1.push("1001");
+arr_pg1.push(root+"大藝術家.mp3");
+arr_pg1.push(art+"蔡依林.jpg");
+arr_pg1.push("大藝術家");
+arr_pg1.push("蔡依林");
+
+arr_pg1.push("1002");
+arr_pg1.push(root+"唯舞獨尊.mp3");
+arr_pg1.push(art+"蔡依林.jpg");
+arr_pg1.push("唯舞獨尊");
+arr_pg1.push("蔡依林");
+
+arr_pg1.push("1003");
+arr_pg1.push(root+"舞孃.mp3");
+arr_pg1.push(art+"蔡依林.jpg");
+arr_pg1.push("舞孃");
+arr_pg1.push("蔡依林");
+
+arr_pg1.push("1004");
+arr_pg1.push(root+"我呸.mp3");
+arr_pg1.push(art+"蔡依林.jpg");
+arr_pg1.push("我呸");
+arr_pg1.push("蔡依林");

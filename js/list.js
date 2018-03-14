8f62cb0a749511cc778599ace7d19b4e4fb79c6c
codelite9006 <codelite9006@gmail.com>
2018-03-14 10:54:00 +0800

    Update list.js

--- js/list.js
+++ js/list.js
@@ -9,67 +9,55 @@ arr_pg1.push("Faded");
 arr_pg1.push("Alan Walker");
 
 arr_pg1.push("1002");
-arr_pg1.push("http://dl.20track.ir/mahdi/Album2/MARS2017/Charlie%20Puth%20-%20Attention%20-%20MP3%20128.mp3");
-arr_pg1.push("http://youth.newagebd.net/wordpress/wp-content/uploads/2017/05/Song.gif");
-arr_pg1.push("Attention");
-arr_pg1.push("Charlie Puth");
-
-arr_pg1.push("1003");
 arr_pg1.push("http://tegos.kz/new/mp3_full/Sia_-_Cheap_Thrills.mp3");
 arr_pg1.push("https://i.pinimg.com/736x/3d/c0/c1/3dc0c120a6cfa21db550b28e08357431--maddie-ziegler-tim-beta.jpg");
 arr_pg1.push("Cheap Thrills");
 arr_pg1.push("Sia");
 
-arr_pg1.push("1004");
+arr_pg1.push("1003");
 arr_pg1.push("http://dl2.djring.com/sd1.djring.com/128/474893/Closer+-+The+Chainsmokers%20(DJJOhAL.Com).mp");
 arr_pg1.push("https://upload.wikimedia.org/wikipedia/en/a/a5/Closer_%28featuring_Halsey%29_%28Official_Single_Cover%29_by_The_Chainsmokers.png");
 arr_pg1.push("Closer");
 arr_pg1.push("The Chainsmokers");
 
-arr_pg1.push("1005");
+arr_pg1.push("1004");
 arr_pg1.push("http://bossmp3.me/siteuploads/7/362/1198/Love%20Me%20Like%20You%20Do%20(Ellie%20Goulding)%20-%20320%20Kbps%20-%20(BossMp3.Me).mp3");
 arr_pg1.push("http://1.bp.blogspot.com/-xVFj92sUzFI/VLFG3pN0KGI/AAAAAAAALq8/wA9ivYedHCQ/s1600/Ellie%2BGoulding%2B-%2BLove%2BMe%2BLike%2BYou%2BDo.jpg");
 arr_pg1.push("Love Me Like You Do");
 arr_pg1.push("Ellie Goulding");
 
-arr_pg1.push("1006");
+arr_pg1.push("1005");
 arr_pg1.push("http://dl2.djring.com/lq.djring.com/48/482473/I%20m%20The%20One%20Ft%20Justin%20Bieber%20Quavo%20Chance%20The%20Rapper%20%20Lil%20Wayne%20-%20DJ%20Khaled%20(DJJOhAL.Com).mp3");
 arr_pg1.push("https://www.lilwaynehq.com/images/blog/dj-khaled-im-the-one-lil-wayne-justin-bieber-chance-the-rapper-quavo-artwork.jpg");
 arr_pg1.push("I'm The One");
 arr_pg1.push("Justin Bieber");
 
-arr_pg1.push("1007");
+arr_pg1.push("1006");
 arr_pg1.push("http://dl2.djring.com/sd2.djring.com/128/479068/Shape%20of%20You%20-%20Ed%20Sheeran%20(DJJOhAL.Com).mp3");
 arr_pg1.push("https://upload.wikimedia.org/wikipedia/en/b/b4/Shape_Of_You_%28Official_Single_Cover%29_by_Ed_Sheeran.png");
 arr_pg1.push("Shape Of You");
 arr_pg1.push("Ed Sheeran"); 
 
-arr_pg1.push("1008");
+arr_pg1.push("1007");
 arr_pg1.push("http://dl2.djring.com/sd1.djring.com/128/474895/Cold+Water+-+Major+Lazer+Justin+Bieber%20(DJJOhAL.Com).mp3");
 arr_pg1.push("http://coverlandia.net/sites/default/files/styles/music_artwork_page/public/artworks/music/38711471968021.png");
 arr_pg1.push("Cold Water");
 arr_pg1.push("Justin Bieber");
 
-arr_pg1.push("1009");
+arr_pg1.push("1008");
 arr_pg1.push("http://tegos.kz/new/mp3_full/Clean_Bandit_-_Rockabye.mp3");
 arr_pg1.push("https://img.discogs.com/wnJDCY_xI9BtICDCOq9oJx6e66c=/fit-in/600x520/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-9444679-1480705235-2916.jpeg.jpg");
 arr_pg1.push("Rockabye");
 arr_pg1.push("Clean Bandit");
 
-arr_pg1.push("1010");
+arr_pg1.push("1009");
 arr_pg1.push("http://dl2.djring.com/sd1.djring.com/128/479065/Let+Me+Love+You+-+DJ+Snake+Ft+Justin+Bieber%20(DJJOhAL.Com).mp3");
 arr_pg1.push("http://media.socastsrm.com/wordpress/wp-content/blogs.dir/69/files/2016/07/bieber.jpg");
 arr_pg1.push("Let Me Love You");
 arr_pg1.push("Justin Bieber");
 
-arr_pg1.push("1011");
+arr_pg1.push("1010");
 arr_pg1.push("http://tentechaudio.com/application/lagubarat/Charlie%20Puth%20(Feat.%20Selena%20Gomez)%20-%20We%20Don't%20Talk%20Anymore.mp3");
 arr_pg1.push("https://images-na.ssl-images-amazon.com/images/M/MV5BOWQyYmJiOWUtNzkzYS00YWJlLWI5YjgtYTg4MjI0MmM1N2ZkXkEyXkFqcGdeQXVyNjE0ODc0MDc@._V1_SY300_SX300_AL_.jpg");
 arr_pg1.push("We Don't Talk Anymore");
 arr_pg1.push("Charlie Puth");
-
-arr_pg1.push("1012");
-arr_pg1.push("http://skymaza.com/upload_file/1/3647/3663/3666/Linkin%20Park%20-%20Meteora%20-%2013%20-%20Numb-.mp3");
-arr_pg1.push("https://is4-ssl.mzstatic.com/image/thumb/Features/v4/9a/c2/68/9ac26832-eeb4-3854-0c47-33f3e5c11cc8/dj.xkwgleci.jpg/1200x630bb.jpg");
-arr_pg1.push("Numb");
-arr_pg1.push("Linkin Park"); 
